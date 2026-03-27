22587bdf6bac6547b96cc3b4d16ba3f2cf11e2f8
johannesalke <johannesalke@gmail.com>
2026-03-27 14:57:18 +0100

    Basic server exists, can accept messages and relay them to rabbit-mq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"os"
+	"os/signal"
+
+	"github.com/johannesalke/learn-pub-sub/internal/pubsub"
+	"github.com/johannesalke/learn-pub-sub/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	//fmt.Println("Starting Peril server...")
+	con, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Err: %s", err)
+	}
+	defer con.Close()
+	fmt.Print("You are now con-nec-ted.\n")
+
+	chan1, err := con.Channel()
+	handleErr(err)
+	err = pubsub.PublishJSON(chan1, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	handleErr(err)
+
+	//wait for ctrl+c?
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
+
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
 }
